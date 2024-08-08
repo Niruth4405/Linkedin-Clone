a5beb82b864eb4e1ed9dba9c660a106f20f9c8f6
Niruth Ananth <niruthananth03@gmail.com>
2024-08-08 11:50:29 +0530

    .

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Card from "./components/Card";
 import Image from "next/image";
+import Home from "../app/Home/page"
 
 export default function Home() {
   return (
     <>
-    <Card />
+    <Home/>
     </>
   );
 }
